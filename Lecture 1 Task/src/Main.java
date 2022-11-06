772ddc91df99311544fe9e4e5e03ea13c620cad2
shcherbatuyk <116522320+shcherbatuyk@users.noreply.github.com>
2022-11-06 11:40:09 +0200

    Update Main.java
    Fixed code formatting

--- Lecture 1 Task/src/Main.java
+++ Lecture 1 Task/src/Main.java
@@ -7,17 +7,17 @@ public class Main {
         long c = 100L;
 
         int sub = b - a;
-        int sum =  a+b;
-        float div = (float)b/a;
-        double mod = b%a;
+        int sum = a + b;
+        float div = (float) b / a;
+        double mod = b % a;
         short inc = ++a;
 
         //Results output
         System.out.println("Sub: " + sub);
-        System.out.println("Sum: "+ sum);
+        System.out.println("Sum: " + sum);
         System.out.println("Formatted Div: " + new DecimalFormat("#0.00").format(div));
-        System.out.println("Mod: " + mod );
-        System.out.println("Some func with long: " + (c -(b+a)));
+        System.out.println("Mod: " + mod);
+        System.out.println("Some func with long: " + (c - (b + a)));
         System.out.println("Inc: " + inc);
     }
 }
